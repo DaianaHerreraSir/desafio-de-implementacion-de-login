df724634d44263061f2efa6ebbcb13153ba89560
Daiana Herrera Sir <dherrerasir@gmail.com>
2024-02-03 18:31:41 +0100

    elimine mi connect mongo

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,7 +5,7 @@ export const connectDB= async () => {
         try {
     
             await mongoose.connect(
-                "mongodb+srv://dherrerasir:mongodb2024@cluster0.2cm3474.mongodb.net/ecommerce?retryWrites=true&w=majority")
+                "")
     
             console.log('Base de datos conectada')        
     
